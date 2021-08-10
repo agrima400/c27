64a2d2cf281a52d54a55abd026afb3ef0d453461
agrima3113 <agrima3113@gmail.com>
2021-08-10 13:35:07 +0530

    i have created constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +39,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6=new Log(230,130,80,PI/2);
+    chain1 = new chain(bird.body, log6.body);
+
 }
 
 function draw(){
@@ -61,4 +67,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain1.display();
 }
